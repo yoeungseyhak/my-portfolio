910b228ed2832661dbca84507e25b43d7497d07d
Hakk <seyhakyoeung@gmail.com>
2025-10-19 03:49:43 +0700

    Updated

--- lib/extension/text_extension.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:portfolio/extension/texttheme_extension.dart';
-
-extension TextExtension on String {
-  Text get textDarkGreyW400S9 =>
-      Text(this, style: Get.textTheme.darkGreyW400S9);
-  Text get textWhiteW400S9 => Text(this, style: Get.textTheme.whiteW400S9);
-}
