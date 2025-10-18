fcff58904c2060ccfd65bcf9b33712a4a50e79d5
Hakk <seyhakyoeung@gmail.com>
2025-10-19 01:48:35 +0700

    Updated

--- lib/extension/size_extension.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-extension SizeExtension on num {
-  SizedBox get width => SizedBox(width: toDouble());
-  SizedBox get height => SizedBox(height: toDouble());
-}
--- lib/helper/regex.dart
+++
@@ -1,3 +0,0 @@
-final String uppercaseRex = r'[A-Z]';
-final String lowercaseRex = r'[a-z]';
-final String numeralRex = r'[0-9]';
